23269c997c7b82a828da8dea136048415c2ef055
HelenCVM <52894597+HelenCVM@users.noreply.github.com>
2020-04-07 11:47:54 -0500

    tabla

---
+++ funciones.js
@@ -0,0 +1,50 @@
+function cargarDatos()
+{
+    var titulo = document.getElementById("titulo").value;
+    var detalles="";
+    if(titulo == ""){
+        detalles="<tr>" +
+        "<td colspan='5' > No hay informacion disponible </td>" +
+        "</tr>";
+        document.getElementById("tablaDatosPersonalesDetalles").innerHTML=detalles;
+
+    }else {
+        if(window.XMLHttpRequest){
+            xmlhttp =new XMLHttpRequest();
+
+        }else {
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        xmlhttp.onreadystatechange =function(){
+            if(this.readyState == 4 && this.status ==200){
+                var data=JSON.parse(this.responseText)
+
+                data.Search.forEach(movie => {
+                    detalles += "<tr>" +
+                    "<td><a href='#' onclick=\"buscarPeliculaPorId('" + movie.imdbID + "' )\">Detalles" +
+                    "<td>" + movie.Title + "</td>" +
+                    "<td>" + movie.Year + "</td>" +
+                    "<td>" + movie.Type + "</td>" +
+                    "<td><img src=" + movie.Poster + "></td>" +
+                    "</tr>";
+                    
+                });
+                document.getElementById("tablaDatosPersonalesDetalles").innerHTML= detalles;
+            }
+        };
+        xmlhttp.open("GET","http://www.omdbapi.com/?apikey=bae08925&s=" + titulo ,true);
+        xmlhttp.send();
+    }
+    return false;
+
+
+}
+
+function buscarPeliculaPorId(id){
+    var detalles="";
+    if(id==""){
+        detalles="No hay informacion disponible"
+        document.getElementById("tablaDatosPersonalesDetalles").innerHTML=detalles;
+    }
+}
